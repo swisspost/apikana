2f4c6c6db48d5f20951b7c65673ed36a7f3efcf8
Stefan Niederhauser <ghuder5@gmx.ch>
2018-01-04 19:49:25 +0100

    fix json schema generation (see #2)

--- src/generate-schema.js
+++ src/generate-schema.js
@@ -10,38 +10,34 @@ var generateEnv = require('./generate-env');
 
 module.exports = {
     generate: function (tsconfig, files, dest, dependencyPath) {
-        var deps = path.resolve(dependencyPath).toLowerCase();
         fse.mkdirsSync(schemaDir('v3'));
         fse.mkdirsSync(schemaDir('v4'));
 
         var schemas = schemaGen.generate(tsconfig, files);
+        var schemaInfos = schemaInfos(schemas);
         for (var name in schemas) {
-            var schema = schemas[name];
-            var fromDependency = path.relative(deps, schema.filename).substring(0, 3) === 'ts/';
-            log('Found ' + (fromDependency ? 'foreign' : 'own') + ' definition', colors.magenta(name));
-            if (!fromDependency) {
-                //TODO definitions could be turned into $refs but attention with types of the same name from different dependencies
-                //TODO -> structural comparision needed
-                // var extRef = {};
-                // extRef[name] = true;
-                // for (var type in schema.definitions) {
-                //     var def = schema.definitions[type];
-                //     if (def.type === 'object' || def.enum) {
-                //         delete schema.definitions[type];
-                //         extRef[type] = true;
-                //     }
-                // }
-                // schemaGen.processRefs(schema, function (ref) {
-                //     if (ref.substring(0, 14) === '#/definitions/' && extRef[ref.substring(14)]) {
-                //         return schemaName(ref.substring(14));
-                //     }
-                //     return ref;
-                // });
+            var info = schemaInfos[name];
+            if (info.source === '') {
+                log('Found definition', colors.magenta(name));
+                var schema = schemas[name];
+                for (var type in schema.definitions) {
+                    var def = schema.definitions[type];
+                    if (schemaInfos[type] && (def.type === 'object' || def.enum)) {
+                        delete schema.definitions[type];
+                    }
+                }
+                schemaGen.processRefs(schema, function (ref) {
+                    var info = schemaInfos[ref.substring(14)];
+                    //TODO types of the same name from different dependencies need structural comparision to find the right source!
+                    if (ref.substring(0, 14) === '#/definitions/' && info.source != null && info.object) {
+                        return info.source + schemaName(ref.substring(14));
+                    }
+                    return ref;
+                });
                 if (params.javaPackage()) {
                     schema.javaType = params.javaPackage() + '.' + schema.id;
                     schema.javaInterfaces = ['java.io.Serializable'];
                 }
-                // schema.definedIn = myProject();
                 delete schema.filename;
                 fs.writeFileSync(schemaFile(name, 'v4'), JSON.stringify(schema, null, 2));
                 convertToV3(schema);
@@ -49,10 +45,21 @@ module.exports = {
             }
         }
 
-        // function myProject() {
-        //     var vars = generateEnv.variables();
-        //     return vars.name || (vars.project && vars.project.artifactId) || path.parse(path.resolve('.')).name;
-        // }
+        function schemaInfos(schemas) {
+            var deps = path.resolve(dependencyPath);
+            var relDeps = path.relative(path.resolve(dest, 'model/json-schema'), deps);
+            var infos = {};
+            for (var name in schemas) {
+                var schema = schemas[name];
+                var rel = path.relative(deps.toLowerCase(), schema.filename);
+                var source = rel.substring(0, 3) === 'ts/' ? relDeps + '/json-schema-v3/' + path.dirname(rel.substring(3)) + '/' : '';
+                infos[name] = {
+                    source: source,
+                    object: schema.type === 'object' || schema.enum
+                };
+            }
+            return infos;
+        }
 
         function schemaFile(type, version) {
             return path.resolve(schemaDir(version), schemaName(type));
