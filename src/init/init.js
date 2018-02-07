b818115e0f6ce044f9f7ac907c67a70423edc60c
Stefan Niederhauser <stefan.niederhauser.3@post.ch>
2018-02-07 18:22:09 +0100

    update version refs

--- src/init/init.js
+++ src/init/init.js
@@ -49,7 +49,7 @@ function init(name, isGlobal, forNode, opts) {
         fse.copySync(__dirname + '/template/maven', name);
         var pom = fs.readFileSync(name + '/pom.xml').toString();
         // opts.version = myPack.version;
-        opts.version = '0.4.0'; //TODO hardcode or myPack.version?
+        opts.version = '0.4.1'; //TODO hardcode or myPack.version?
         opts.global = isGlobal;
         for (var opt in opts) {
             pom = pom.replace('%' + opt + '%', opts[opt]);
