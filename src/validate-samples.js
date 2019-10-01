c161c3ceb9462539cf4db6e1191f9fb0d1ec5bcf
Mattiamato <mattiamato@gmail.com>
2019-10-01 09:57:46 +0200

    Added improvements

--- src/validate-samples.js
+++ src/validate-samples.js
@@ -1,6 +1,8 @@
+var gulp = require('gulp');
+var jeditor = require("gulp-json-editor");
 const path = require('path');
 const Ajv = require('ajv');
-const fs = require('fs');
+const fse = require('fs-extra');
 const winston = require('winston');
 const util = require('util');
 
@@ -19,7 +21,7 @@ module.exports = {
         });
 
         function createAjv() {
-            const schemaFiles = fs.readdirSync(path.join(root, 'dist', 'model', 'json-schema-v4'));
+            const schemaFiles = fse.readdirSync(path.join(root, 'dist', 'model', 'json-schema-v4'));
 
             // verbose: Include input data information for Schema validation errors
             const ajv = new Ajv({ schemaId: 'id', verbose: true });
@@ -55,7 +57,7 @@ module.exports = {
 
             if (!valid) {
                 logger.warn(`${fileName} is NOT valid`);
-                logger.warn(util.format(validate.errors));
+                validate.errors.forEach(error => logger.warn(error.dataPath + ' ' + util.format(error.message)));
                 validationResult.filesWithError.push(fileName);
             } else {
                 logger.info(`${fileName} is valid`);
@@ -72,7 +74,7 @@ module.exports = {
                 filesWithError: []
             };
 
-            fs.readdirSync(path.join(root, 'src', 'samples'))
+            fse.readdirSync(path.join(root, 'src', 'samples'))
                 .filter(file => dataFilePattern.test(file))
                 .map(file => {
                     const match = dataFilePattern.exec(file);
@@ -120,10 +122,49 @@ module.exports = {
             }
         }
 
-        validateTestDataFiles();
+        function task(name, deps, func) {
+            if (!func) {
+                func = deps;
+                deps = [];
+            }
+            gulp.task(name, deps, function () {
+                var start = Date.now();
+                var first;
+                return func()
+                    .on('readable', function () {
+                        if (!first) {
+                            first = Date.now();
+                        }
+                    })
+                    .on('finish', function () {
+                        logger.info(`Done in ${first ? (Date.now() - first) / 1000 : (Date.now() - start) / 1000} s`);
+                    })
+                    .on('error', function (err) {
+                        logger.error(`Error: ${err}`);
+                    });
+            });
+        }
 
+        task('copy-samples', function() {
+            logger.info('Cleanup dist/samples');
+            // cleanup dist/samples and copy all src/samples again
+            fse.removeSync(path.join(root, 'dist', 'samples'));
+            logger.info('Copying samples from src/samples to dist/samples.');
+            return gulp.src('samples/**', {cwd:  path.join(root, 'src')}).pipe(jeditor(json => {
+                if('$schema' in json) {
+                    delete json['$schema'];
+                }
+                return json;
+            })).pipe(gulp.dest('samples', {cwd: path.join(root, 'dist')}));
+        });
 
-    }
-        
+        task('validate', ['copy-samples'], function() {
+            // Before validate, copy the samples to ensure they are up-to-date
+            validateTestDataFiles();
+            return gulp.src([]);
+        });
+
+        gulp.start();
+    } 
 };
 
