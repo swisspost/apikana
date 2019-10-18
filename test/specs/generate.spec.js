be822edb84a0a0483359dcf13c0d9df9778b141b
Laurent Bovet <laurent.bovet@windmaster.ch>
2019-10-18 13:41:01 +0200

    Improving test structure

--- test/specs/generate.spec.js
+++ test/specs/generate.spec.js
@@ -1,36 +1,44 @@
 const fs = require('fs-extra');
 const sandbox = require('./sandbox');
 
-describe('generate', () => {
+describe('generating', () => {
 
-    beforeAll(() => sandbox.init()
-        .then(() =>
-            sandbox.scaffold({
-                type: 'stream-api',
-                domain: 'acme.org',
-                author: 'coyote',
-                namespace: 'garden.pet',
-                shortName: 'garden-pet',
-                projectName: 'garden-pet-stream-api',
-                title: 'Garden Pet Stream API',
-                plugins: ['maven', 'dotnet'],
-                javaPackage: 'org.acme.garden.pet.v1',
-                mavenGroupId: 'org.acme.garden',
-                dotnetNamespace: 'Org.Acme.Garden.Pet',
-                dotnetPackageId: 'Org.Acme.Garden.Pet.StreamApi',
-                mqs: 'Kafka'
-            })));
-    afterAll(sandbox.clean);
+    describe('an API', () => {
 
-    it('should generate dist', () => sandbox.generate()
-        .then(({dir}) => fs.exists(`${dir}/dist`, res => res || fail())))
+        var generated = null;
 
-    it('should copy default-types in dist', () => sandbox.generate()
-        .then(({dir}) => fs.exists(`${dir}/dist/model/ts/node_modules/apikana/default-types.ts`, res => res || fail())))
+        beforeAll(() => sandbox.init()
+            .then(() =>
+                sandbox.scaffold({
+                    type: 'stream-api',
+                    domain: 'acme.org',
+                    author: 'coyote',
+                    namespace: 'garden.pet',
+                    shortName: 'garden-pet',
+                    projectName: 'garden-pet-stream-api',
+                    title: 'Garden Pet Stream API',
+                    plugins: ['maven', 'dotnet'],
+                    javaPackage: 'org.acme.garden.pet.v1',
+                    mavenGroupId: 'org.acme.garden',
+                    dotnetNamespace: 'Org.Acme.Garden.Pet',
+                    dotnetPackageId: 'Org.Acme.Garden.Pet.StreamApi',
+                    mqs: 'Kafka'
+                })
+            .then(sandbox.generate)
+            .then(result => { generated = result })));
+        afterAll(sandbox.clean);
 
-    it('should copy version number in generated API', () => sandbox.generate()
-        .then(({dir}) => JSON.parse(fs.readFileSync(`${dir}/dist/model/openapi/api.json`).toString('utf8')))
-        .then(api => expect(api.info.version).toBe('0.1.0-rc.1')))
+        it('should generate dist', () =>
+            fs.exists(`${generation.dir}/dist`, res => res || fail()))
+
+        it('should copy default-types in dist', () =>
+            fs.exists(`${generation.dir}/dist/model/ts/node_modules/apikana/default-types.ts`, res => res || fail()))
+
+        it('should copy version number in generated API', () => sandbox.generate()
+            .then(({dir}) => JSON.parse(fs.readFileSync(`${dir}/dist/model/openapi/api.json`).toString('utf8')))
+            .then(api => expect(api.info.version).toBe('0.1.0-rc.1')))
+
+    })
 })
 
 
