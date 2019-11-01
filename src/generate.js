7343c932fbbf82eefc73f75059ea0a181f8657db
Laurent Bovet <laurent.bovet@windmaster.ch>
2019-11-01 11:35:14 +0100

    Fix inlining of dependencies in complete API files

--- src/generate.js
+++ src/generate.js
@@ -447,7 +447,7 @@ module.exports = {
             completeApi.definitions = {};
             delete completeApi.definitions.$ref;
             var fileToType = {};
-            return gulp.src('model/json-schema-v4/**/*.json', {cwd: dest})
+            return gulp.src([ dest+'/model/json-schema-v4/**/*.json', dependencyPath+'/json-schema-v4/**/*.json'])
                 .pipe(through.obj(function (file, enc, cb) {
                     var schema = JSON.parse(file.contents.toString());
                     fileToType[path.parse(file.path).base] = schema.id;
--- test/specs/generate.spec.js
+++ test/specs/generate.spec.js
@@ -22,7 +22,7 @@ describe('generating', () => {
                     dotnetPackageId: 'Org.Acme.Garden.Pet.StreamApi',
                     mqs: 'Kafka'
                 })
-            .then(sandbox.generate))
+                    .then(sandbox.generate))
             .then(result => { dir = result.dir }));
         afterAll(sandbox.clean);
 
@@ -42,9 +42,64 @@ describe('generating', () => {
                 expect(api.info.version)
                     .toBe('0.1.0-rc.1'));
         })
+
+        describe('an API with a dependency', () => {
+            const gulp = require('gulp');
+            const depSandbox = require('./sandbox')();
+            const mainSandbox = require('./sandbox')();
+            var depDir;
+            var mainDir;
+            beforeAll(() => depSandbox.init()
+                .then(() =>
+                    depSandbox.scaffold({
+                        type: 'rest-api',
+                        domain: 'acme.org',
+                        author: 'coyote',
+                        namespace: 'wild.pet',
+                        shortName: 'wild-pet',
+                        projectName: 'wild-pet-rest-api',
+                        title: 'Wild Pet Rest API',
+                        plugins: []
+                    }))
+                .then(depSandbox.generate)
+                .then(result => { depDir = result.dir })
+                .then(() => mainSandbox.init())
+                .then(() =>
+                    mainSandbox.scaffold({
+                        type: 'rest-api',
+                        domain: 'acme.org',
+                        author: 'coyote',
+                        namespace: 'zoo.pet',
+                        shortName: 'zoo-pet',
+                        projectName: 'zoo-pet-rest-api',
+                        title: 'Zoo Pet Rest API',
+                        plugins: []
+                    }))
+                .then(result => { mainDir = result.dir })
+                .then(() => new Promise(resolve => fs.writeFile(`${mainDir}/src/ts/pet.ts`,`
+                import {Pet} from "wild-pet-rest-api/pet";
+                export interface PetList {
+                    pets: Pet[]
+                }
+                `, () => resolve())))
+                .then(() =>
+                    new Promise(resolve =>
+                        gulp.src('src/ts/**/*.ts', {cwd: depDir})
+                            .pipe(gulp.dest('node_modules/-api-dependencies/ts/wild-pet-rest-api', {cwd: mainDir})).on('finish', resolve)))
+                .then(() =>
+                    new Promise(resolve =>
+                        gulp.src('dist/model/json-schema-v4/*.json', {cwd: depDir})
+                            .pipe(gulp.dest('node_modules/-api-dependencies/json-schema-v4/wild-pet-rest-api', {cwd: mainDir})).on('finish', resolve)))
+                .then(mainSandbox.generate));
+
+
+            afterAll(depSandbox.clean);
+            afterAll(mainSandbox.clean);
+
+            fit('generates a complete API file with inlined dependencies', () => {
+                expect(JSON.parse(fs.readFileSync(`${dir}/dist/model/openapi/complete-api.json`).toString('utf8'))
+                    .definitions.Pet.properties.firstName.type).toBe('string');
+            })
+        })
     })
 })
-
-
-
-
