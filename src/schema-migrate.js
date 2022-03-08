0779decc72a79c7dbaabba75b186421b43158fb1
Amato Mattia, I235 <mattia.amato@post.ch>
2022-03-08 11:30:36 +0100

    Fix json schema $id migration

--- src/schema-migrate.js
+++ src/schema-migrate.js
@@ -7,11 +7,24 @@ module.exports = {
 
         // change the target latest schema version to migrate to here
         migrator.draft7(schemaCopy);
-        // should prepend a # before the id value to make it a valid URI
-        if (schemaCopy['$id'] && !schemaCopy['$id'].startsWith('#')) {
-            schemaCopy['$id'] = '#' + schemaCopy['$id']
+        // should prepend a # before the id value to make it a valid URI at schema root
+        schemaCopy['$id'] = this.prefixWith('#', schemaCopy['$id']);
+
+        // should prepend a # before the id value to make it a valid URI at every schema definition object
+        if (schemaCopy.definitions) {
+            for (const [_, defValue] of Object.entries(schemaCopy.definitions)) {
+                defValue['$id'] = this.prefixWith('#', defValue['$id']);
+            }
         }
 
         return schemaCopy;
+    },
+
+    // Prepend a prefix to data only in case it is not yet the case
+    prefixWith: function (prefix, data) {
+        if (data && !data.startsWith(prefix)) {
+           return prefix + data;
+        }
+        return data;
     }
 }
